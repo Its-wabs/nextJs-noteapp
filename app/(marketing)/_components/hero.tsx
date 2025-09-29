ec9e741fe3c2b7f6f539349bf1c380effa034c3e
normydevbop <cherfi.m.abdelwahab@gmail.com>
2025-09-29 18:00:29 +0100

    update:landing page pictures

--- app/(marketing)/_components/hero.tsx
+++ app/(marketing)/_components/hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
             <div className="flex items-center">
                 <div className=" relative w-[300x] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
 <Image
-            src="/landing-dark.png"
+            src="/landing-dark"
             
             alt="logo"
             className="dark:hidden"
